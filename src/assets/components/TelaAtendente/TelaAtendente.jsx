4bee7da86e862554701062fd688e2d28b07db5a1
lukaafff <luizagarcia2405@gmail.com>
2023-08-20 00:40:21 -0300

    Tela atendente, tela login e sidebar
    Alteração de estilos, correção do sidebar e adição de uma lógica for para os cards

--- src/assets/components/TelaAtendente/TelaAtendente.jsx
+++ src/assets/components/TelaAtendente/TelaAtendente.jsx
@@ -1,105 +1,38 @@
-import './TelaAtendente.css'
-
+import './TelaAtendente.css';
 
 const TelaAtendente = () => {
 
-  const fakeDataCard = [
-    {
-      id: 1,
-      task: `card 1`,
-      state: 'available'
-    },
-    {
-      id: 2,
-      task: `card 2`,
-      state: 'available'
-    },
-    {
-      id: 3,
-      task: `card 3`,
-      state: 'available'
-    },
-    {
-      id: 4,
-      task: `card 4`,
-      state: 'busy'
-    },
-    {
-      id: 5,
-      task: `card 5`,
-      state: 'available'
-    },
-    {
-      id: 6,
-      task: `card 6`,
-      state: 'available'
-    },
-    {
-      id: 7,
-      task: `card 7`,
-      state: 'busy'
-    },
-    {
-      id: 8,
-      task: `card 8`,
-      state: 'busy'
-    },
-    {
-      id: 9,
-      task: `card 9`,
-      state: 'busy'
-    },
-    {
-      id: 10,
-      task: `card 10`,
-      state: 'available'
-    },
-    {
-      id: 11,
-      task: `card 11`,
-      state: 'busy'
-    },
-    {
-      id: 12,
-      task: `card 12`,
-      state: 'busy'
-    },
-    {
-      id: 13,
-      task: `card 13`,
-      state: 'available'
-    },
-    {
-      id: 14,
-      task: `card 14`,
-      state: 'available'
-    },
-    {
-      id: 15,
-      task: `card 15`,
-      state: 'busy'
-    },
-]
-    
+  const generateFakeData = (count) => {
+    const data = [];
+    for (let i = 1; i <= count; i++) {
+      data.push({
+        id: i,
+        task: `card ${i}`,
+        state: i % 3 === 0 ? 'busy' : 'available'
+      });
+    }
+    return data;
+  };
+
+  const fakeDataCard = generateFakeData(50);
+
   return (
     <>
-         <nav className="navigation">
-            <div className="nav-search">
-              <label htmlFor="">Buscar: </label>
-                <input type="search" placeholder="Numero da Comanda"/>
-            </div>
-         </nav>
+      <nav className="navigation">
+        <div className="nav-search">
+          <label htmlFor="">Buscar: </label>
+          <input type="search" placeholder="Numero da Comanda"/>
+        </div>
+      </nav>
 
-         <main className='main-container'>
-       
-          {fakeDataCard.map((card) => (
-              <div className={`card ${card.state}`}>
-                  <h1> {card.id} </h1>
-                  <p> {card.task} </p>
-              </div>
-            ))}
-      
-         </main>
+      <main className='main-container'>
+        {fakeDataCard.map((card) => (
+          <div className={`card ${card.state}`} key={card.id}>
+            <h1> {card.id} </h1>
+            <p> {card.task} </p>
+          </div>
+        ))}
+      </main>
     </>
   )
 }
