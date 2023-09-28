177362d47a303ce4f2dae6b3d03159ae971931c7
lukaafff <luizagarcia2405@gmail.com>
2023-09-28 17:28:22 -0300

    atualização

--- src/assets/components/TelaLogin/Login.jsx
+++ src/assets/components/TelaLogin/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import "./Login.css"
 import logo from "../../images/logoRedondaMelinda.png"
-import useFetchLogin from "../../../store/useFetchLogin";
+import useFetchLogin from "../../../hooks/useFetchLogin";
 import AddFuncionarios from "../../routes/Funcionarios/AddFuncionarios";
 
 function Login() {
